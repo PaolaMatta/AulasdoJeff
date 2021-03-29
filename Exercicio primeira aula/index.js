60363c66746ffea3343f7cb54fce45c372ff6df1
PaolaMatta <paolacm@usp.br>
2021-03-29 18:29:12 -0300

    Exercicio feito

---
+++ Exercicio primeira aula/index.js
@@ -0,0 +1,23 @@
+/* Let serve para declarar qualquer variável, seja ela number, string, boolean e outras. Já o const serve para declarar variáveis que sejam constantes, isto é, que tenham valores pré-definidos, como o valor de PI que sempre será 3,14.
+*
+* 2) 
+* a) 5 strings */ 
+let nomeDoGato = "Lucky";
+let corDaCamiseta = "Vermelha";
+let saborDoSuco = "Limão";
+let mensagemPadrão1 = "Inscreva-se no nosso site!";
+let mensagemPadrãoClima = "Céu com nuvens com possibilidade de chuva no fim da tarde";
+/*
+* b) 5 booleans */
+let pequenoDemais = false;
+let grandeDemais = true;
+let acimaDe5 = false;
+let maiorQue10 = false;
+let maisDe3Lados = false;
+/*
+* c) 5 numbers */
+const diaDeNascimentoPaola = 13;
+let alturaEmCm = 157;
+let númeroDeSentenças = 2.300;
+let sentencasFavoráveis = 50;
+let preçoDaGarrafa = 1;
